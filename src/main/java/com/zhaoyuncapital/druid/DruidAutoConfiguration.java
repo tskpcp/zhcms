bd9c3ec481fe4b60efe06b1606dbf4e4b3c0158c
tskpcp <tskpcp@126.com>
2017-07-23 17:43:21 +0800

    update

--- src/main/java/com/zhaoyuncapital/druid/DruidAutoConfiguration.java
+++ src/main/java/com/zhaoyuncapital/druid/DruidAutoConfiguration.java
@@ -16,36 +16,36 @@ import java.sql.SQLException;
 /**
  * Created by gongtuo on 2017/5/22.
  */
-@Configuration
-@EnableConfigurationProperties(DruidProperties.class)
-@ConditionalOnClass(DruidDataSource.class)
-@ConditionalOnProperty(prefix = "druid", name = "url")
-@AutoConfigureBefore(DataSourceAutoConfiguration.class)
-public class DruidAutoConfiguration {
-    @Autowired
-    private DruidProperties properties;
-
-    @Bean
-    public DataSource dataSource() {
-        DruidDataSource dataSource = new DruidDataSource();
-        dataSource.setUrl(properties.getUrl());
-        dataSource.setUsername(properties.getUsername());
-        dataSource.setPassword(properties.getPassword());
-        if (properties.getInitialSize() > 0) {
-            dataSource.setInitialSize(properties.getInitialSize());
-        }
-        if (properties.getMinIdle() > 0) {
-            dataSource.setMinIdle(properties.getMinIdle());
-        }
-        if (properties.getMaxActive() > 0) {
-            dataSource.setMaxActive(properties.getMaxActive());
-        }
-        dataSource.setTestOnBorrow(properties.isTestOnBorrow());
-        try {
-            dataSource.init();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        return dataSource;
-    }
-}
+//@Configuration
+//@EnableConfigurationProperties(DruidProperties.class)
+//@ConditionalOnClass(DruidDataSource.class)
+//@ConditionalOnProperty(prefix = "druid", name = "url")
+//@AutoConfigureBefore(DataSourceAutoConfiguration.class)
+//public class DruidAutoConfiguration {
+//    @Autowired
+//    private DruidProperties properties;
+//
+//    @Bean
+//    public DataSource dataSource() {
+//        DruidDataSource dataSource = new DruidDataSource();
+//        dataSource.setUrl(properties.getUrl());
+//        dataSource.setUsername(properties.getUsername());
+//        dataSource.setPassword(properties.getPassword());
+//        if (properties.getInitialSize() > 0) {
+//            dataSource.setInitialSize(properties.getInitialSize());
+//        }
+//        if (properties.getMinIdle() > 0) {
+//            dataSource.setMinIdle(properties.getMinIdle());
+//        }
+//        if (properties.getMaxActive() > 0) {
+//            dataSource.setMaxActive(properties.getMaxActive());
+//        }
+//        dataSource.setTestOnBorrow(properties.isTestOnBorrow());
+//        try {
+//            dataSource.init();
+//        } catch (SQLException e) {
+//            throw new RuntimeException(e);
+//        }
+//        return dataSource;
+//    }
+//}
